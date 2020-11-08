d3a7ccddce60d1d073e3caa92dac7613a19c0266
sushilmate <sushil.mate@outlook.com>
2020-11-08 21:01:25 +0100

    Adding new methods. for, foreach, & linq

--- BenchmarkingCSharpCode/Program.cs
+++ BenchmarkingCSharpCode/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BenchmarkingCSharpCode
 {
@@ -6,7 +10,63 @@ namespace BenchmarkingCSharpCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summery = BenchmarkRunner.Run<PerformanceChecker>();
+
+            Console.WriteLine(summery);
+            Console.ReadLine();
+        }
+    }
+
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class PerformanceChecker
+    {
+        public PerformanceChecker()
+        {
+            FillArray();
+        }
+
+        [GlobalSetup]
+        private void FillArray()
+        {
+            var random = new Random();
+            items = new List<int>(10000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                items.Add(random.Next(1, 20000));
+            }
+        }
+
+        public List<int> items { get; set; }
+                
+        [Benchmark]
+        public long SumByLinq()
+        {
+            long sum = items.Sum();
+            return sum;
+        }
+
+        [Benchmark]
+        public long SumByForLoop()
+        {
+            long sum = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                sum += items[i];
+            }
+            return sum;
+        }
+
+        [Benchmark]
+        public long SumByForeach()
+        {
+            long sum = 0;
+            foreach (var item in items)
+            {
+                sum += item;
+            }
+            return sum;
         }
     }
 }
